e7d596e358eeaa35859b0b30605ca3842ac4c634
wnsgk <wnsgk@DESKTOP-BJ139D3>
2019-08-07 13:38:19 +0900

    userController 에러 수정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,12 +1,14 @@
-var form = document.getElementById('userupdateForm');
 
-function updateUser() {
-	form.submit();
-}
 
-function deleteUser() {
-	form.submit();
-}
+$(document).ready(function() {
+    $('#btnUpdateUser').click(function(){
+    	$('#userUpdateForm').append("<input type='hidden' id='putOrDelete' name='_method' value='PUT'/>");
+    });
+
+    $('#btnDeleteUser').click(function(){
+    	$('#userUpdateForm').append("<input type='hidden' id='putOrDelete' name='_method' value='DELETE'/>");
+    });
+})
 
 function logout() {
 	location.href = "/logout";
